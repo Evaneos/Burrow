4c3298b3ffd5267aca72f5dca16f7a0f3c484dca
Matthieu Verrecchia <matthieu.verrecchia@gmail.com>
2015-01-05 14:00:43 +0100

    Add async examples

--- examples/03-async-sender.php
+++ examples/03-async-sender.php
@@ -1,6 +1,12 @@
 #!/usr/bin/php
 <?php
 
+if (!isset($argv[1])) {
+    $io = fopen('php://stderr', 'w+');
+    fwrite($io, "usage : php 03-async-sender.php nbevents\n");
+    die;
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $amqpService = new \Burrow\RabbitMQ\AmqpService('127.0.0.1', 5672, 'guest', 'guest');
@@ -12,4 +18,3 @@ $event = new \Burrow\Event('image.resize', array('name' => 'msg'));
 for ($i = 0; $i < $argv[1]; ++$i) {
     $dispatcher->dispatch($event);
 }
-
